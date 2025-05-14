3ad04229ae86f1a5778ab3a064b6cd1d56f2f55d
Eric Hooks <ebhooks21@gmail.com>
2025-05-14 12:58:14 -0500

    Added function to check for extra command line arguments.

--- VHostCreator.js
+++ VHostCreator.js
@@ -4,9 +4,14 @@
  */
 
 import os from 'os';
+import CommandLineProcessor from './app/CommandLineProcessor.js';
+
+const clp = new CommandLineProcessor();
 
 //Output the program tagline
 console.log(`VHostCreator -- Program for generating Apache VHOSTS files.${os.EOL}`);
+
+clp.checkForCommandLineArguments();
 
 //Exit the program
 process.exit(0);
