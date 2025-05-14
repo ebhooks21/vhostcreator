a682a46d34a5f767a10aacfee4b3b89636c43c80
Eric Hooks <ebhooks21@gmail.com>
2025-05-14 13:12:24 -0500

    Created the function for displaying the help menu.

--- app/CommandLineOutput.js
+++ app/CommandLineOutput.js
@@ -3,11 +3,24 @@
  * Author: Eric Hooks, 2025
  */
 
+import os from 'os';
+
 export default class CommandLineOutput {
 	/**
 	 * Constructor for the CommandLineOutput class.
 	 */
 	constructor() {
 
+	}
+
+	/**
+	 * Function for displaying the help menu.
+	 */
+	displayHelpMenu() {
+		console.log(`Help Listing: ${os.EOL}`);
+		console.log(os.EOL);
+
+		console.log(`Possible Arguments: ${os.EOL}`);
+		console.log(`--h -- Displays this help message.${os.EOL}`);
 	}
 }
