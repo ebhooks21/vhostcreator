50e932fe3b78aad842507eed80320af1b4270024
Eric Hooks <ebhooks21@gmail.com>
2025-05-14 13:09:27 -0500

    Created the CommandLineOutput class.

---
+++ app/CommandLineOutput.js
@@ -0,0 +1,13 @@
+/**
+ * CommandLineOutput.js -- File to hold the CommandLineOutput class, which holds functions for displaying menus for the application.
+ * Author: Eric Hooks, 2025
+ */
+
+export default class CommandLineOutput {
+	/**
+	 * Constructor for the CommandLineOutput class.
+	 */
+	constructor() {
+
+	}
+}
