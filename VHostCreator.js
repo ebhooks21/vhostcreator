5a3b3253f007f7a5c5ae264c8ccaa5c7788a2d18
Eric Hooks <ebhooks21@gmail.com>
2025-05-14 12:45:22 -0500

    Added the program tagline.

---
+++ VHostCreator.js
@@ -0,0 +1,12 @@
+/**
+ * VHostCreator.js -- Main entry point for the VHostCreator application.
+ * Author: Eric Hooks, 2025
+ */
+
+import os from 'os';
+
+//Output the program tagline
+console.log(`VHostCreator -- Program for generating Apache VHOSTS files.${os.EOL}`);
+
+//Exit the program
+process.exit(0);
