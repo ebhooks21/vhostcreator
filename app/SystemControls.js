105483c44ecddd3ddbafac30177aa0f6d9796567
Eric Hooks <ebhooks21@gmail.com>
2025-05-14 20:48:13 -0500

    Started the SystemControls class to interface the program with the system.

---
+++ app/SystemControls.js
@@ -0,0 +1,16 @@
+/**
+ * SystemControls.js -- File to contain the SystemControls class, which has the functionality for dealing with the system.
+ * Author: Eric Hooks, 2025 
+ */
+
+import os from 'os';
+import fs from 'fs';
+
+export default class SystemControls {
+	/**
+	 * Constructor for the SystemControls class.
+	 */
+	constructor() {
+
+	}
+}
